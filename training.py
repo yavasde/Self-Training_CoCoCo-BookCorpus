aba731b698b3320eb5abd6163877f038f33d9ea3
yavasde <56029511+yavasde@users.noreply.github.com>
2025-02-19 11:35:51 +0100

    Update training.py

--- training.py
+++ training.py
@@ -14,7 +14,7 @@ and F1 score.
 2. Load training and test datasets from pickle files.
 3. Train an initial SVM classifier using the original T-PAS training data.
 4. Test the classifier on the original test set from T-PAS (English) and 
-an additional test set (ACL).
+an additional test set (BookCorpus).
 5. Perform self-training by iterating over a corpus, predicting probabilities, 
 and selectively 
    adding high-confidence predictions to the training data.
@@ -37,15 +37,15 @@ def test_classifier(clf, test_data):
 
 
 probability = 0.9
-predication_type = "Information"
+predication_type = "Food" #or Event
 
 # Load Data
 with open(f"data/{predication_type}_TPAS.pickle", "rb") as data_file:
     TPAS_train = pickle.load(data_file)["train"]
 with open(f"data/{predication_type}_test_EN.pickle", "rb") as data_file:
     TPAS_test = pickle.load(data_file)
-ACL_test = ...  # load the test data
-ACL_corpus = ...  # load the ACL corpus
+BookCorpus_test = ...  # load the test data
+BookCorpus = ...  # load the Book Corpus 
 
 # Training a classifier with the original data (T-PAS)
 clf = svm.SVC(gamma=0.001, C=100.0, probability=True)
@@ -54,13 +54,13 @@ clf.fit(TPAS_train["vectors"], TPAS_train["labels"])
 # Test the classifier on the original test set
 test_classifier(clf, TPAS_test)
 
-# Test the classifier on ACL test set
-test_classifier(clf, ACL_test)
+# Test the classifier on BookCorpus test set
+test_classifier(clf, BookCorpus_test)
 
 # Self-training loop starts here. Turn this to a loop:
 
-# Apply the classifier to ACL corpus and check the classifier probability and select the sentences based on that
-for sentence in ACL_corpus:
+# Apply the classifier to BookCorpus corpus and check the classifier probability and select the sentences based on that
+for sentence in BookCorpus:
     relation_vector = ...  # get the relation vector for each sentence
     y_pred_verb = clf.predict_proba(
         np.asarray(relation_vector, "float64").reshape(1, -1)
@@ -78,8 +78,8 @@ for sentence in ACL_corpus:
 # Test the classifier on the original test set
 test_classifier(new_clf, TPAS_test)
 
-# Test the classifier on ACL test set
-test_classifier(new_clf, ACL_test)
+# Test the classifier on BookCorpus test set
+test_classifier(new_clf, BookCorpus_test)
 
 # Check if we should stop the self-training loop and save the best classifier
 # Edit Here
